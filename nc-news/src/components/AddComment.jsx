0da7023b7694a8c068c6d01ce258f33bc420afe7
Rob Lehane <roblehane@Rob-Lehanes-MacBook-Pro.local>
2023-10-23 17:39:13 +0100

    more changes to comments

--- nc-news/src/components/AddComment.jsx
+++ nc-news/src/components/AddComment.jsx
@@ -3,7 +3,7 @@ import { postNewComment } from '../utils/api.js'
 import {useState} from 'react'
 
 function AddComment(id){
-    const [confirmation, setConfirmation] = useState(<></>)
+    const [confirmation, setConfirmation] = useState(null)
     const [comment, setComment] = useState('')
 
     const handleSubmit = (e) => {
@@ -18,7 +18,7 @@ function AddComment(id){
     }
 
     const handleReset = () => {
-        setConfirmation(<></>)
+        setConfirmation(null)
         setComment(null)
     }
 
@@ -32,8 +32,15 @@ function AddComment(id){
             name="comment_box" 
             placeholder="Type your comment here" 
             onChange = {(e) => setComment(e.target.value)} 
+            disabled = {confirmation}
             required/>
-            <button type="submit" onClick={handleSubmit}>Submit comment</button> <button type="reset" onClick={handleReset}>Reset</button>
+            <button 
+            type="submit" 
+            onClick={handleSubmit} 
+            disabled={confirmation}>Submit comment</button> 
+            <button 
+            type="reset" 
+            onClick={handleReset}>Reset</button>
             {confirmation}
         </form>
         </>
