5756b61ccb1fdf0bb90e098d5f93083f48f15d3d
Rob Lehane <roblehane@Rob-Lehanes-MacBook-Pro.local>
2023-10-25 11:57:01 +0100

    completed delete comment

--- nc-news/src/utils/api.js
+++ nc-news/src/utils/api.js
@@ -48,7 +48,7 @@ export const getTopics = () => {
 
 export const deleteComment = (comment_id) => {
     return newsApi.delete(`/comments/${comment_id}`).then((res)=> {
-        console.log(res.status)
+        console.log(res.status, "comment deleted successfully")
         return res.status;
     })
 }
